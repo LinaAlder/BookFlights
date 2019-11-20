2880c0e73cd81ef85183293b1b979d26ee33f02d
Polina Vasilyeva <50369251+LinaAlder@users.noreply.github.com>
2019-11-20 14:27:16 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bookflights/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.bookflights;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        String selected = spinner.getSelectedItem().toString();
+        Toast.makeText(getApplicationContext(), selected, Toast.LENGTH_SHORT).show();
+
+    }
+}
